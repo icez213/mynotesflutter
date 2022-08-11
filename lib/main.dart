e5bf0970b35f56fb45ac61a7eb88a505e2e34b85
Icez <kpto.isaac@gmail.com>
2022-08-11 19:18:38 +0800

    initial comit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
+import 'package:mynotes/views/login_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const Homepage(),
+  ));
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user =
+                  FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                return const Text('You are verified');
+              } else {
+                return const Text(
+                    'You need to veify ur email');
+              }
+            default:
+              return const Text('Loading...');
+          }
+        },
+      ),
+    );
+  }
+}
